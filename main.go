8c8d0fff3b392029ce9e738cb71c8f98d3c1093f
bigerlab <bigerlab@126.com>
2021-09-03 22:37:04 +0800

    dssdsfdsdf

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ func main() {
 	r.GET("/syncd/me", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"branch": "buga",
+			"time":time.Now().Format("2006-01-02 15:04:05"),
 		})
 	})
 	r.Run("0.0.0.0:9080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
