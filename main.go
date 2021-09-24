2c756632c1103147c1cf9f81e2a48a6996a50a4f
bigerlab <bigerlab@126.com>
2021-09-24 15:40:22 +0800

    5666

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ func main() {
 	})
 	router.GET("/syncd/me", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"branch": "古董 500",
+			"branch": "hello 500",
 			"time":time.Now().Format("2006-01-02 15:04:05"),
 		})
 	})
